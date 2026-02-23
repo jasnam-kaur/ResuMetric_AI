1e3608ace9545eb210b6b52c39e150cb1b9c22c9
jasnam-kaur <167450835+jasnam-kaur@users.noreply.github.com>
2026-02-23 17:30:19 +0530

    Add Render domain to ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-6&47i_0g2un_kdetrj0p*xf#2)sho^okvgv$&iy%*4p6y2j3%1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['resumetric-ai.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
