cfc79c61b10c196fa2afd437b98e8911f304ab1c
jasnam-kaur <167450835+jasnam-kaur@users.noreply.github.com>
2026-03-01 12:52:54 +0530

    build.sh fix

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# 1. Install standard requirements
 pip install -r requirements.txt
 
-# This command replaces the need for that broken URL
-python -m spacy download en_core_web_sm
+# 2. Force install the specific spaCy model directly
+# This bypasses URL resolution issues in requirements.txt
+pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.7.0/en_core_web_sm-3.7.0-py3-none-any.whl
 
+# 3. Rest of your build commands
 python -c "import nltk; nltk.download('punkt'); nltk.download('wordnet'); nltk.download('omw-1.4')"
 python manage.py collectstatic --no-input
 python manage.py migrate
