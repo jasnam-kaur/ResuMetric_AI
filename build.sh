a19aa7df3c92416f4ffc4868ee70f6b4f26b61cd
jasnam-kaur <167450835+jasnam-kaur@users.noreply.github.com>
2026-03-01 12:39:59 +0530

    build.sh and yaml file fix

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
+
 pip install -r requirements.txt
-python -m spacy download en_core_web_sm 
+
+# This command replaces the need for that broken URL
+python -m spacy download en_core_web_sm
+
+python -c "import nltk; nltk.download('punkt'); nltk.download('wordnet'); nltk.download('omw-1.4')"
 python manage.py collectstatic --no-input
 python manage.py migrate
